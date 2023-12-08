658da8736853996bbbb8e4d2e27abdece8027a92
Nguyen Nguyen <nltn@msn.com>
2023-12-08 07:59:47 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Installs N, the Node.js version manager, on Unix-like platforms, 
+# without needing to install Node.js first.
+if [ ! -d "$HOME/n" ]; then
+    curl -s -L http://git.io/n-install | bash -s -- -y
+fi
+
+# Install the latest Node.js version
+sudo "$HOME/n/bin/n" latest
+
+# Update to the latest version of the NPM package manager
+sudo npm update --global
+
+# Install smee-client
+sudo npm install --global smee-client
